267118f8798c58d95853a58ec2ece0061340ecf7
13168311313 <ealohu@163.com>
2018-05-31 14:12:42 +0800

    完成自动注入对象功能

--- app.py
+++ app.py
@@ -2,22 +2,32 @@
 # 启动类
 import json
 from flask import Flask
-from controller import TestData,ShopGoodsController
 from flask import jsonify
 from flask import request
 import mysql
 from mysql import Pool
+from annotation import AutoWired
+from properties import IOCProp
 
 _app=Flask(__name__)
 _app.config['JSON_AS_ASCII']=False
 
 _SGCobj=None
 
-# 获取顶部标题栏
-@_app.route('/tabs',methods=['GET'])
-def hello():
-    result=TestData.getHeadTitle()
-    return jsonify(result)
+obj_list=IOCProp.obj_list
+
+# 注入对象
+# @AutoWired.Wired([ShopGoodsController.ShopGoodsController],a_w_list=['_SGCobj'])
+@AutoWired.AutoWired(obj_list)
+def inject_obj():
+    pass
+
+#
+# # 获取顶部标题栏
+# @_app.route('/tabs',methods=['GET'])
+# def hello():
+#     result=TestData.getHeadTitle()
+#     return jsonify(result)
 
 # 获取商品列表
 @_app.route('/shop/goods/list',methods=['post'])
@@ -39,13 +49,10 @@ def getGoodIntroduction():
 
 
 
-
-
 # 启动服务器
 if '__main__'==__name__:
     print('加载数据库模块')
     mysql.pool=Pool.Pool()
     print('加载完毕')
-    _SGCobj=ShopGoodsController.ShopGoodsController()
     _app.run(host='0.0.0.0',port=443,ssl_context=(mysql.project_path+'/sslContext/1_zxyzt.cn_bundle.crt',mysql.project_path+'/sslContext/2_zxyzt.cn.key'))
     # _app.run(host='0.0.0.0',port=443,ssl_context='adhoc')
---
+++ properties/IOCProp.py
@@ -0,0 +1,4 @@
+
+from controller import TestData,ShopGoodsController
+
+obj_list={'_SGCobj':ShopGoodsController.ShopGoodsController}
