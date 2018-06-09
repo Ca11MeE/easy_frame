2854255d9a308358cf3dab78c465a18e6cebc8cc
CallMeE <ealohu@163.com>
2018-06-09 20:52:44 +0800

    完成新装饰器功能并优化流程: 功能:根据请求方式自动装配参数,添加该装饰器后,可直接在方法写参数名称(免除从request中不断获取),提高代码可读性 待实现:以指定名称装配参数

--- annotation/__init__.py
+++ annotation/__init__.py
@@ -1,6 +1,8 @@
 # coding: utf-8
-from flask import jsonify
+from flask import jsonify, request
 import functools
+import sys
+import os
 
 """
 注解集合(部分)
@@ -8,10 +10,11 @@ author:CallMeE
 date:2018-06-01
 
 
-json形式返回数据修饰器(跳过视图解析器)
 """
 
 
+# 响应返回数据修饰器(跳过视图解析器)
+# 响应体形式返回
 def ResponseBody():
     def method(f):
         @functools.wraps(f)
@@ -26,3 +29,54 @@ def ResponseBody():
         return arg
 
     return method
+
+
+# 处理请求参数装饰器
+# 表单形式数据
+def AutoParam(kwarg_list=[]):
+    def method(f):
+        # print(locals())
+        @functools.wraps(f)
+        def args():
+            if 'GET' == str(request.method):
+                return is_get(f,request,kwarg_list)
+            elif 'POST' == str(request.method):
+                return is_post(f, request, kwarg_list)
+            else:
+                sys.stderr.write('方法不支持!!')
+        return args
+
+    return method
+
+def is_get(f,r,kw_list):
+    r_arg = ()
+    r_kwarg = {}
+    if not kw_list:
+        r_arg=r.args.to_dict().values()
+    else:
+        for index in range(len(kw_list)):
+            r_kwarg[kw_list[index]] = r.args[kw_list[index]]
+    return f(*r_arg,**r_kwarg)
+
+def is_post(f,r,kw_list):
+    r_arg=()
+    r_kwarg={}
+    if r.is_json:
+        if not kw_list:
+            r_arg=is_json(r.json)
+        else:
+            for index in range(len(kw_list)):
+                r_kwarg[kw_list[index]] = r.json[kw_list[index]]
+    else:
+        if not kw_list:
+            r_arg=is_form(r.form)
+        else:
+            for index in range(len(kw_list)):
+                r_kwarg[kw_list[index]] = r.form[kw_list[index]]
+    return f(*r_arg, **r_kwarg)
+
+def is_json(arg_list):
+    return arg_list.values()
+
+def is_form(arg_list):
+    return arg_list.to_dict().values()
--- routes/ShopGoodsRoute.py
+++ routes/ShopGoodsRoute.py
@@ -1,11 +1,12 @@
 # coding: utf-8
 # 启动类
-from flask import request,Blueprint
+from flask import request, Blueprint
 import annotation
 import boot
 # from controller import ShopGoodsController
 from annotation import AutoWired
 from properties import IOCProp
+
 """
 蓝图DEMO
 author:CallMeE
@@ -13,36 +14,41 @@ date:2018-06-01
 """
 
 # 此处定义蓝图前缀
-url_prefix=None
-app = Blueprint('app',__name__,url_prefix=url_prefix)
+url_prefix = None
+app = Blueprint('app', __name__, url_prefix=url_prefix)
 
 obj_list = IOCProp.obj_list
 
-_SGCobj=None
+_SGCobj = None
+
 
 # 注入对象
 # @AutoWired.InnerWired([ShopGoodsController.ShopGoodsController],a_w_list=['_SGCobj'],g=globals())
-@AutoWired.OuterWired(obj_list,g=globals())
+@AutoWired.OuterWired(obj_list, g=globals())
 def inject_obj():
     # for name in obj_list.keys():
     #     globals()[name]=AutoWired.get_obj(name)
     pass
 
+
 inject_obj()
 
+
 # 获取顶部标题栏
-@app.route('/tabs', methods=['GET'])
+@app.route('/tabs', methods=['GET', 'POST'])
+@annotation.AutoParam()
 @annotation.ResponseBody()
-def hello():
+def hello(a, b):
     result = _SGCobj.getHeadTitle()
     return result
 
 
 # 获取商品列表
 @app.route('/shop/goods/list', methods=['post'])
+@annotation.AutoParam()
 @annotation.ResponseBody()
-def getShopGoodsList():
-    result = _SGCobj.findGoodsList(request.json['page'], request.json['pageSize'])
+def getShopGoodsList(page, pageSize):
+    result = _SGCobj.findGoodsList(page, pageSize)
     return result
 
 
@@ -61,6 +67,7 @@ def getGoodIntroduction():
     result = _SGCobj.findGoodIntroduction(request.json['id'])
     return result
 
-@app.route('/',methods=['get'])
+
+@app.route('/', methods=['get'])
 def index():
     return '<button>点击一下吧</button>'
